8ce1ab1d8275d9e1e906795cbcf069e639ade849
Shadedog838 <32343626+Shadedog838@users.noreply.github.com>
2018-03-17 11:07:14 -0400

    update garbotronv1.0

---
+++ itemList.js
@@ -0,0 +1,67 @@
+var clicked=localStorage.clicked;
+var garbageItem=JSON.parse(localStorage[clicked]);
+if(localStorage.length) {
+document.querySelector('#title').textContent=garbageItem.title;
+document.querySelector('#sum').textContent=garbageItem.sum;
+document.querySelector('#stepOneimg').src=garbageItem.stepOneimg;
+document.querySelector('#para1').textContent=garbageItem.para1;
+document.querySelector('#item1').textContent=garbageItem.item1;
+document.querySelector('#item2').textContent=garbageItem.item2;
+document.querySelector('#stepTwoimg').src=garbageItem.stepTwoimg;
+document.querySelector('#para2').textContent=garbageItem.para2;
+document.querySelector('#item11').textContent=garbageItem.item11;
+document.querySelector('#stepThreeimg').src=garbageItem.stepThreeimg;
+document.querySelector('#para3').textContent=garbageItem.para3;
+document.querySelector('#item13').textContent=garbageItem.item13;
+document.querySelector('#stepFourimg').src=garbageItem.stepFourimg;
+document.querySelector('#para4').textContent=garbageItem.para4;
+document.querySelector('#stepFiveimg').src=garbageItem.stepFiveimg;
+document.querySelector('#para5').textContent=garbageItem.para5;
+document.querySelector('#item15').textContent=garbageItem.item15;
+document.querySelector('#item25').textContent=garbageItem.item25;
+document.querySelector('#stepSiximg').src=garbageItem.stepSiximg;
+document.querySelector('#para6').textContent=garbageItem.para6;
+document.querySelector('#stepSevenimg').src=garbageItem.stepSevenimg;
+document.querySelector('#para7').textContent=garbageItem.para7;
+}
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#Done').addEventListener('click', function() {
+  var one=document.querySelector("#One");
+  var two=document.querySelector("#Two");
+  var three=document.querySelector("#Three");
+  var four=document.querySelector("#Four");
+  var five=document.querySelector("#Five");
+  var six=document.querySelector("#Six");
+    var seven=document.querySelector("#Seven");
+  if(one.checked!==true){
+
+    window.alert("You didn't do step 1");
+  }else if(two.checked!==true){
+    window.alert("You didn't do step 2");
+  }else if(three.checked!==true){
+    window.alert("You didn't do step 3");
+  }else if(four.checked!==true){
+    window.alert("You didn't do step 4");
+  }else if(five.checked!==true){
+    window.alert("You didn't do step 5")
+  }else if(six.checked!==true){
+    window.alert("You didn't do step 6")
+  }else if(seven.checked!==true){
+    window.alert("You didn't do step 6")
+  }else{
+    document.querySelector("#Modal").style.display="block";
+
+  }
+
+
+
+});
---
+++ script.js
@@ -0,0 +1,99 @@
+var antifreeze={
+  title:"How to Dispose of Antifreeze",
+
+sum:"Antifreeze, a necessary chemical for vehicles, is very dangerous. Check with your government to find out how to dispose of antifreeze waste. Take used antifreeze to a recycling center that accepts it. Antifreeze tainted with oil or gas must be shipped with documentation to a hazardous waste facility. Clean up any spills right away by covering them with an absorbent material like kitty litter.",
+stepOneimg:"https://www.wikihow.com/images/thumb/7/74/Dispose-of-Antifreeze-Step-1.jpg/aid9007404-v4-728px-Dispose-of-Antifreeze-Step-1.jpg.webp",
+
+para1:"Contact recycling centers for antifreeze disposal locations. Check with your local government or visit a community recycling day. They may accept pure antifreeze or direct you where to go. Other good places to check are machine shops and oil change shops. Many of them accept antifreeze for recycling. Wastewater management places may also accept small amounts of antifreeze.",
+item1:"Search for facilities by sedoaarching your city’s website, searching online for mechanics in your area, or calling disposal companies in the area.",
+item2:"There may be a small facility usage fee when recycling antifreeze. These fees will be greater at hazardous waste disposal facilities.",
+stepTwoimg:"https://www.wikihow.com/images/thumb/3/3b/Dispose-of-Antifreeze-Step-2.jpg/aid9007404-v4-728px-Dispose-of-Antifreeze-Step-2.jpg.webp",
+para2:"Identify antifreeze that looks dirty or smells like gas. Even a drop of oil or gas in antifreeze taints it. This tainted antifreeze is considered hazardous and must be shipped to a different facility than normal used antifreeze. Look at it for signs of muddiness. Normal antifreeze is brightly-colored and smells sweet.",
+item11:"Damage to a vehicle can cause fluids to mix. If you aren’t sure, consider the antifreeze polluted.",
+stepThreeimg:"https://www.wikihow.com/images/thumb/b/b9/Dispose-of-Antifreeze-Step-3.jpg/aid9007404-v4-728px-Dispose-of-Antifreeze-Step-3.jpg.webp",
+para3:"Place tainted and pure waste antifreeze into separate containers. Store the antifreeze in containers such as old antifreeze bottles. Make sure these containers are well-sealed. Both types of antifreeze must be shipped to different places, so label them to remember which one is which.",
+item13:"When flushing antifreeze out of a vehicle, use a different drain pan and funnel than you use for oil and other fluids.",
+stepFourimg:"https://www.wikihow.com/images/thumb/d/df/Dispose-of-Antifreeze-Step-4.jpg/aid9007404-v4-728px-Dispose-of-Antifreeze-Step-4.jpg.webp",
+para4:"Find hazardous waste disposal centers for tainted antifreeze. This antifreeze can’t be recycled. Since it is considered hazardous, only a hazardous waste disposal facility will accept it. Ask your local government. Mechanics at repair shops may also be able to tell you where they dispose of their antifreeze.",
+stepFiveimg:"https://www.wikihow.com/images/thumb/9/9c/Dispose-of-Antifreeze-Step-5.jpg/aid9007404-v4-728px-Dispose-of-Antifreeze-Step-5.jpg.webp",
+para5:"Drive the antifreeze to a recycling facility. You can’t ship antifreeze in the mail. You have to bring the container to the facility. A commercial hauler or waste oil service may also do it for you. Once you reach the facility, get a receipt that shows where you delivered the antifreeze.",
+item15:"Hiring a commercial hauler for hazardous antifreeze makes the process easier. This is only necessary for shipping large amounts of antifreeze. If you transport it yourself, you will need to provide your own documentation about using the truck.",
+item25:"Always consult your government for disposal regulations.",
+}
+var fluids={
+  title:"How to Dispose of Fluids",
+
+sum:"The question of how to go about the disposal of car fluids when they have been changed is not one to be taken lightly. If not properly disposed, these car fluids will end up contaminating the environment, resulting in serious health hazards for humans and animals alike. Some of the car fluids that are frequently replaced include automotive oil, antifreeze, gear oil, automatic transmission fluid, and power steering fluid. Fortunately, many of these car fluids can be recycled into new products.",
+stepOneimg:"http://www.cummingutilities.com/wp-content/uploads/2016/01/waste-disposal.jpg",
+para1:"Contact recycling centers for antifreeze disposal locations. Check with your local government or visit a community recycling day. They may accept pure antifreeze or direct you where to go. Other good places to check are machine shops and oil change shops. Many of them accept antifreeze for recycling. Wastewater management places may also accept small amounts of antifreeze.",
+stepTwoimg:"https://i.ytimg.com/vi/t3TbcM-M-98/hqdefault.jpg",
+para2:"Reusing them: some car fluids like antifreeze and automotive engine coolant can actually be stored in special containers and reused later.",
+stepThreeimg:"http://images.newcars.com/images/car-pictures/original/2014-Subaru-XV-Crosstrek-SUV-2.0i-Premium-4dr-All-wheel-Drive-Photo.png",
+para3:"Burning them off: car fluids like automotive oil and other oil based car fluids can be burned off to reduce their chances of contaminating the environment as a result of poor storage conditions.",
+stepFourimg:"https://images-na.ssl-images-amazon.com/images/I/41NZxNNx6%2BL.jpg",
+para4:"Some car fluids are highly toxic to the environment and require storage in special leak-proof containers to minimize the chances of spillage and contamination before handing them over to the hazardous waste processing facility within your locality. Some of these wastes include gasoline, brake fluids and transmission fluids.",
+para5:"THERE IS NO DIRECTIONS HERE JUST CHECK OFF THE BOX",
+
+}
+var motoroil={
+  title:"How to Dispose of Motor Oil",
+  sum:"Disposing of used motor oil and oil filters in the garbage is illegal in many jurisdictions. Due to environmental concerns, it is also illegal to burn the oil or dump it into a drain. If you change your own oil, you will need to learn how to responsibly recycle your used oil and oil filter. Fortunately, this is becoming easier and more accessible as environmental concerns grow. Learning how to recycle a used oil filter is a matter of following a few simple steps.",
+  stepOneimg:"https://www.wikihow.com/images/thumb/4/4d/Recycle-a-Used-Oil-Filter-Step-1.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-1.jpg.webp",
+  para1:"Drain your car's oil into a drain pan. Before you begin an oil change, get a pan ready to collect the oil as it pours from your car. A disposable aluminum baking pan works well for this, but any wide, shallow pan will work in a pinch.",
+  stepTwoimg:"https://www.wikihow.com/images/thumb/c/ce/Recycle-a-Used-Oil-Filter-Step-2.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-2.jpg.webp",
+  para2:"Clean up any spills during the oil change with an absorbent material. If you spill any oil onto your work area, do not hose down the area to clean it up. Instead, spread an absorbent material like sawdust or cat litter on the spilled oil, and then bag the soaked material in a leak-proof bag for recycling.",
+  stepThreeimg:"https://www.wikihow.com/images/thumb/7/7f/Recycle-a-Used-Oil-Filter-Step-3.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-3.jpg.webp",
+para3:"Pour the used oil from your pan into a plastic container. Saving the original container is the easiest method, but any clean plastic container with a tight-fitting lid will work. Use a funnel when filling the container to minimize spills. Do not add any other waste liquids to the oil. Contaminated oil cannot be recycled; it must be treated as hazardous waste.",
+stepFourimg:"https://www.wikihow.com/images/thumb/a/a8/Recycle-a-Used-Oil-Filter-Step-4.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-4.jpg.webp",
+para4:"Place your used oil filter in a sealed plastic bag. Once you remove your old oil filter, bag it carefully in a leak-proof bag with a tight seal. Large zip-top bags work well for this.",
+stepFiveimg:"https://www.wikihow.com/images/thumb/b/ba/Recycle-a-Used-Oil-Filter-Step-5.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-5.jpg.webp",
+para5:"Store the used oil and oil filter properly until you are able to recycle them. Mark each plastic bag with the words waste oil, and store them away from direct sunlight. Aim to take them to a recycling center as soon as possible.",
+stepSiximg:"https://www.wikihow.com/images/thumb/7/72/Recycle-a-Used-Oil-Filter-Step-6.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-6.jpg.webp",
+para6:"Locate an oil recycling center near you. Many quick lube businesses and service stations will recycle your used oil for you, so call nearby locations to see if that is an option. Many local governments also run oil recycling programs. In the United States, both the American Petroleum Institute and Earth911.org maintain resources that can help you locate a recycling center near you.",
+stepSevenimg:"https://www.wikihow.com/images/thumb/3/34/Recycle-a-Used-Oil-Filter-Step-7.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-7.jpg.webp",
+para7:"Take your used oil and oil filter to the recycling center. As long as you have stored the oil in leak-proof bags or containers, you should have no problem handing them off to the recycling center. If you are driving the used oil to the recycling center, be sure to store the oil in the trunk during the drive, not in the cabin.",
+}
+var oilfilter={
+  title:"How to Dispose of Oil Filter",
+  sum:"Disposing of used motor oil and oil filters in the garbage is illegal in many jurisdictions. Due to environmental concerns, it is also illegal to burn the oil or dump it into a drain. If you change your own oil, you will need to learn how to responsibly recycle your used oil and oil filter. Fortunately, this is becoming easier and more accessible as environmental concerns grow. Learning how to recycle a used oil filter is a matter of following a few simple steps.",
+  stepOneimg:"https://www.wikihow.com/images/thumb/4/4d/Recycle-a-Used-Oil-Filter-Step-1.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-1.jpg.webp",
+  para1:"Drain your car's oil into a drain pan. Before you begin an oil change, get a pan ready to collect the oil as it pours from your car. A disposable aluminum baking pan works well for this, but any wide, shallow pan will work in a pinch.",
+  stepTwoimg:"https://www.wikihow.com/images/thumb/c/ce/Recycle-a-Used-Oil-Filter-Step-2.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-2.jpg.webp",
+  para2:"Clean up any spills during the oil change with an absorbent material. If you spill any oil onto your work area, do not hose down the area to clean it up. Instead, spread an absorbent material like sawdust or cat litter on the spilled oil, and then bag the soaked material in a leak-proof bag for recycling.",
+  stepThreeimg:"https://www.wikihow.com/images/thumb/7/7f/Recycle-a-Used-Oil-Filter-Step-3.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-3.jpg.webp",
+para3:"Pour the used oil from your pan into a plastic container. Saving the original container is the easiest method, but any clean plastic container with a tight-fitting lid will work. Use a funnel when filling the container to minimize spills. Do not add any other waste liquids to the oil. Contaminated oil cannot be recycled; it must be treated as hazardous waste.",
+stepFourimg:"https://www.wikihow.com/images/thumb/a/a8/Recycle-a-Used-Oil-Filter-Step-4.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-4.jpg.webp",
+para4:"Place your used oil filter in a sealed plastic bag. Once you remove your old oil filter, bag it carefully in a leak-proof bag with a tight seal. Large zip-top bags work well for this.",
+stepFiveimg:"https://www.wikihow.com/images/thumb/b/ba/Recycle-a-Used-Oil-Filter-Step-5.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-5.jpg.webp",
+para5:"Store the used oil and oil filter properly until you are able to recycle them. Mark each plastic bag with the words waste oil, and store them away from direct sunlight. Aim to take them to a recycling center as soon as possible.",
+stepSiximg:"https://www.wikihow.com/images/thumb/7/72/Recycle-a-Used-Oil-Filter-Step-6.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-6.jpg.webp",
+para6:"Locate an oil recycling center near you. Many quick lube businesses and service stations will recycle your used oil for you, so call nearby locations to see if that is an option. Many local governments also run oil recycling programs. In the United States, both the American Petroleum Institute and Earth911.org maintain resources that can help you locate a recycling center near you.",
+stepSevenimg:"https://www.wikihow.com/images/thumb/3/34/Recycle-a-Used-Oil-Filter-Step-7.jpg/aid1367462-v4-728px-Recycle-a-Used-Oil-Filter-Step-7.jpg.webp",
+para7:"Take your used oil and oil filter to the recycling center. As long as you have stored the oil in leak-proof bags or containers, you should have no problem handing them off to the recycling center. If you are driving the used oil to the recycling center, be sure to store the oil in the trunk during the drive, not in the cabin.",
+
+}
+var gasoline={
+  title:"How to Dispose of Gasoline",
+  sum:"Gasoline is an important part of daily life, but it can pose a serious hazard if not disposed of properly. To safely get rid of your old gas, reach out to your local government authorities for advice. You might need to head to a recycling center, waste disposal site, auto parts store, or even the fire department. When you transport the gas, place it in secure, sealed containers. Also, keep in mind that in some cases it’s possible to recondition your gas for future use instead of disposing of it.",
+  stepOneimg:"https://www.wikihow.com/images/thumb/2/24/Dispose-of-Gasoline-Step-1.jpg/aid9424335-v4-728px-Dispose-of-Gasoline-Step-1.jpg.webp",
+  para1:"Contact your local recycling authority. Some recycling centers will accept gas and recondition it or recycle it safely. Your city’s government officials should be able to direct you to the right recycling place. Then, call the recycling location in advance to see if they have any particular instructions that you’ll need to follow.",
+  stepTwoimg:"https://www.wikihow.com/images/thumb/6/6f/Dispose-of-Gasoline-Step-2.jpg/aid9424335-v4-728px-Dispose-of-Gasoline-Step-2.jpg.webp",
+  para2:"Take it to a hazardous waste disposal center. This is slightly different than recycling because a waste center will usually dispose of, not repurpose, your gasoline. You can still talk with local government management to determine where a waste disposal site is near you. Call ahead to find out restrictions, operating hours, and what they’ll accept.",
+item12:"Some hazardous waste disposal centers are free only to citizens from a particular area, with outsiders forced to pay a fee for disposal.",
+item22:"In less populated areas, your local waste disposal may only be open very limited hours. This is another reason to call ahead.",
+item23:"Centers may also have a maximum amount of gas, such as 10 gallons (37.9 L), that they will accept in a single visit or from an individual over a particular time period.",
+
+}
+var clickables=document.querySelectorAll(".clickable");
+clickables.forEach(function(elem){
+  elem.addEventListener("click", function(event){
+    event.preventDefault();
+    localStorage.clicked=event.target.id;
+    location.href="itemList.html";
+});
+});
+localStorage.antifreeze=JSON.stringify(antifreeze);
+localStorage.fluids=JSON.stringify(fluids);
+localStorage.motorOil=JSON.stringify(motoroil);
+localStorage.oilFilters=JSON.stringify(oilfilter);
+localStorage.gasoline=JSON.stringify(gasoline);
