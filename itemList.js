7e27295e1f1f823c2587218b4f7a2b5d480d0d59
Shadedog838 <32343626+Shadedog838@users.noreply.github.com>
2018-03-24 00:24:25 -0400

    garbotron v1.0 update

--- itemList.js
+++ itemList.js
@@ -23,10 +23,72 @@ document.querySelector('#stepSiximg').src=garbageItem.stepSiximg;
 document.querySelector('#para6').textContent=garbageItem.para6;
 document.querySelector('#stepSevenimg').src=garbageItem.stepSevenimg;
 document.querySelector('#para7').textContent=garbageItem.para7;
+document.querySelector('#item17').textContent=garbageItem.item17;
+document.querySelector('#title1').textContent=garbageItem.title1;
+document.querySelector('#title2').textContent=garbageItem.title2;
+document.querySelector('#title3').textContent=garbageItem.title3;
+document.querySelector('#title4').textContent=garbageItem.title4;
+document.querySelector('#title5').textContent=garbageItem.title5;
+document.querySelector('#title6').textContent=garbageItem.title6;
+document.querySelector('#title7').textContent=garbageItem.title7;
+
+
+
+
+
+}
+var para6= document.querySelector("#para6").textContent;
+var para7= document.querySelector("#para7").textContent;
+var para5= document.querySelector("#para5").textContent;
+var item22= document.querySelector("#item22").textContent;
+
+if(para6==""){
+  document.querySelector("#stepSix").style.display="none";
+}
+if(para7==""){
+    document.querySelector("#stepSeven").style.display="none";
+
+}
+if(para5==""){
+  document.querySelector("#stepFive").style.display="none";
+
+}
+if(item22==""){
+  document.querySelector("#item22").style.display="none";
+}
+var synth = window.speechSynthesis;
+  synth.cancel();
+
+
+document.querySelector('#play').addEventListener('click', readText);
+
+function readText() {
+console.log('text');  var text =garbageItem.title+". "+garbageItem.sum+". "+garbageItem.title1+". "+garbageItem.para1+". "+garbageItem.item1+". "+garbageItem.item2+". "+garbageItem.title2+". "+garbageItem.para2+". "+garbageItem.item12+". "+garbageItem.title3+". "+garbageItem.para3+". "+garbageItem.item13
++". "+garbageItem.title4+". "+garbageItem.para4+". "+garbageItem.title5+". "+garbageItem.para5+". "+garbageItem.item15+". "+garbageItem.item25+". "+garbageItem.title6+". "+garbageItem.para6+". "+garbageItem.title7+". "+garbageItem.para7+". "+garbageItem.item17;
+ if(synth.paused){
+  synth.resume();
+}
+if (text !== '') {
+  var speech = new SpeechSynthesisUtterance(text);
+
+  speech.onerror = function (event) {
+    console.error('SpeechSynthesisUtterance.onerror');
+  }
+
+  var voices = synth.getVoices();
+  speech.voice = voices[0]; // Google US English
+  speech.pitch = 1;
+  speech.rate = 0.9;
+  synth.speak(speech);
+}
 }
 
-
-
+document.querySelector('#pause').addEventListener('click', pauseText);
+function pauseText() {
+  if(synth.speaking){
+    synth.pause();
+  }
+}
 
 
 
@@ -41,28 +103,186 @@ document.querySelector('#Done').addEventListener('click', function() {
   var four=document.querySelector("#Four");
   var five=document.querySelector("#Five");
   var six=document.querySelector("#Six");
+  var ans=document.querySelector("#title").textContent;
     var seven=document.querySelector("#Seven");
-  if(one.checked!==true){
+    if(ans=="How to Dispose of Antifreeze"){
+      if(one.checked!==true){
 
-    window.alert("You didn't do step 1");
-  }else if(two.checked!==true){
-    window.alert("You didn't do step 2");
-  }else if(three.checked!==true){
-    window.alert("You didn't do step 3");
-  }else if(four.checked!==true){
-    window.alert("You didn't do step 4");
-  }else if(five.checked!==true){
-    window.alert("You didn't do step 5")
-  }else if(six.checked!==true){
-    window.alert("You didn't do step 6")
-  }else if(seven.checked!==true){
-    window.alert("You didn't do step 6")
+        window.alert("You didn't do step 1");
+      }else if(two.checked!==true){
+        window.alert("You didn't do step 2");
+      }else if(three.checked!==true){
+        window.alert("You didn't do step 3");
+      }else if(four.checked!==true){
+        window.alert("You didn't do step 4");
+      }else if(five.checked!==true){
+        window.alert("You didn't do step 5");
+
+
   }else{
     document.querySelector("#Modal").style.display="block";
 setTimeout(function(){     location.href="index.html";
 }, 5000);
 
   }
+}
+
+
+
+
+
+});
+document.querySelector('#Done').addEventListener('click', function() {
+  var one=document.querySelector("#One");
+  var two=document.querySelector("#Two");
+  var three=document.querySelector("#Three");
+  var four=document.querySelector("#Four");
+  var five=document.querySelector("#Five");
+  var six=document.querySelector("#Six");
+  var ans=document.querySelector("#title").textContent;
+    var seven=document.querySelector("#Seven");
+    if(ans=="How to Dispose of Fluids"){
+      if(one.checked!==true){
+
+        window.alert("You didn't do step 1");
+      }else if(two.checked!==true){
+        window.alert("You didn't do step 2");
+      }else if(three.checked!==true){
+        window.alert("You didn't do step 3");
+      }else if(four.checked!==true){
+        window.alert("You didn't do step 4");
+      }else{
+    document.querySelector("#Modal").style.display="block";
+setTimeout(function(){     location.href="index.html";
+}, 5000);
+
+  }
+}
+
+
+
+
+
+});
+document.querySelector('#Done').addEventListener('click', function() {
+  var one=document.querySelector("#One");
+  var two=document.querySelector("#Two");
+  var three=document.querySelector("#Three");
+  var four=document.querySelector("#Four");
+  var five=document.querySelector("#Five");
+  var six=document.querySelector("#Six");
+  var ans=document.querySelector("#title").textContent;
+    var seven=document.querySelector("#Seven");
+    if(ans=="How to Dispose of Motor Oil"){
+      if(one.checked!==true){
+
+        window.alert("You didn't do step 1");
+      }else if(two.checked!==true){
+        window.alert("You didn't do step 2");
+      }else if(three.checked!==true){
+        window.alert("You didn't do step 3");
+      }else if(four.checked!==true){
+        window.alert("You didn't do step 4");
+      }else if(five.checked!==true){
+        window.alert("You didn't do step 5");
+      }else if(six.checked!==true){
+        window.alert("You didn't do step 6");
+      }else if(seven.checked!==true){
+        window.alert("You didn't do step 7");
+
+
+
+  }else{
+    document.querySelector("#Modal").style.display="block";
+setTimeout(function(){     location.href="index.html";
+}, 5000);
+
+  }
+}
+
+
+
+
+
+});
+document.querySelector('#Done').addEventListener('click', function() {
+  var one=document.querySelector("#One");
+  var two=document.querySelector("#Two");
+  var three=document.querySelector("#Three");
+  var four=document.querySelector("#Four");
+  var five=document.querySelector("#Five");
+  var six=document.querySelector("#Six");
+  var ans=document.querySelector("#title").textContent;
+    var seven=document.querySelector("#Seven");
+    if(ans=="How to Dispose of Oil Filter"){
+      if(one.checked!==true){
+
+        window.alert("You didn't do step 1");
+      }else if(two.checked!==true){
+        window.alert("You didn't do step 2");
+      }else if(three.checked!==true){
+        window.alert("You didn't do step 3");
+      }else if(four.checked!==true){
+        window.alert("You didn't do step 4");
+      }else if(five.checked!==true){
+        window.alert("You didn't do step 5");
+      }else if(six.checked!==true){
+        window.alert("You didn't do step 6");
+      }else if(seven.checked!==true){
+        window.alert("You didn't do step 7");
+
+
+
+  }else{
+    document.querySelector("#Modal").style.display="block";
+setTimeout(function(){     location.href="index.html";
+}, 5000);
+
+  }
+}
+
+
+
+
+
+});
+document.querySelector('#Done').addEventListener('click', function() {
+  var one=document.querySelector("#One");
+  var two=document.querySelector("#Two");
+  var three=document.querySelector("#Three");
+  var four=document.querySelector("#Four");
+  var five=document.querySelector("#Five");
+  var six=document.querySelector("#Six");
+  var ans=document.querySelector("#title").textContent;
+    var seven=document.querySelector("#Seven");
+    if(ans=="How to Dispose of Gasoline"){
+      if(one.checked!==true){
+
+        window.alert("You didn't do step 1");
+      }else if(two.checked!==true){
+        window.alert("You didn't do step 2");
+      }else if(three.checked!==true){
+        window.alert("You didn't do step 3");
+      }else if(four.checked!==true){
+        window.alert("You didn't do step 4");
+      }else if(five.checked!==true){
+        window.alert("You didn't do step 5");
+      }else if(six.checked!==true){
+        window.alert("You didn't do step 6");
+      }else if(seven.checked!==true){
+        window.alert("You didn't do step 7");
+
+
+
+  }else{
+    document.querySelector("#Modal").style.display="block";
+setTimeout(function(){     location.href="index.html";
+}, 5000);
+
+  }
+}
+
+
 
 
 
